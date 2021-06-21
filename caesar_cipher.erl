0a12bba7a97cf2404a7f65bbeeef97bf8041176c
John Melody Me <Johnmelodyme@yandex.com>
2021-06-21 19:52:33 +0800

    Initial commit

---
+++ caesar_cipher.erl
@@ -0,0 +1,39 @@
+%% [author] : John Melody Me <Johnmelody@dingtalk.com>
+%% [project] : Luhn Algorithm
+%% execute by caesar_cipher:cipher("whaettttt", 23).
+
+-module(caesar_cipher).
+-author("John Melody Me").
+
+-export([cipher/2, key/1, rotate/2]).
+
+key(Key) when Key < 0 ->
+  26 + Key rem 26;
+key(Key) when Key > 25 ->
+  Key rem 26;
+key(Key) ->
+  Key.
+
+rotate(Char, Key) when (Char >= $A) and (Char =< $Z) or
+                        (Char >= $a) and (Char =< $z) ->
+                            Offset = $A + Char band 32,
+                            N = Char - Offset,
+                            Offset + (N + Key) rem 26;
+rotate(Char, _Key) ->
+    Char.
+
+cipher(Plaintext, Key) ->
+    Encode = key(Key),
+    % Decode = key(-Key),
+
+    io:format("Original Text ~s~n", [Plaintext]),
+
+    CypherText = lists:map(
+      fun(Char) -> 
+        rotate(Char, Encode) 
+      end, 
+      Plaintext
+    ),
+
+    % Plaintext = lists:map(fun(Char) -> rot(Char, Decode) end, CypherText), 
+    io:format("Ciphered ~s~n", [CypherText]).
